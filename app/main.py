4ead522a00def23bda3268d73d1e86b792d91c65
Aditya Mewada <eng.adityamewada@gmail.com>
2025-04-29 05:52:08 +0530

    cors fix

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, # Allows specified origins
+    allow_origins=all, # Allows specified origins
     allow_credentials=True,
     allow_methods=["*"], # Allows all methods
     allow_headers=["*"], # Allows all headers
